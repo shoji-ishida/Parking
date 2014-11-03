a6db1cfeb5f30d51cc8f3d48657e0690859a9c2b
shoji-ishida <shoji.ishida@gmail.com>
2014-11-03 11:36:14 +0900

    Initial Commit

---
+++ Parking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parking
+//
+//  Created by ishida on 2014/11/03.
+//  Copyright (c) 2014年 ishida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParkingTests/ParkingTests.m
@@ -0,0 +1,40 @@
+//
+//  ParkingTests.m
+//  ParkingTests
+//
+//  Created by ishida on 2014/11/03.
+//  Copyright (c) 2014年 ishida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ParkingTests : XCTestCase
+
+@end
+
+@implementation ParkingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
